69e86432300a19d4d6aa46780acbb9f33a123bdf
demidov_g@mail.ru <demidov_g@mail.ru>
2021-10-24 18:24:06 +0300

    2

---
+++ Lab4-2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pol = 0;
+            int otr = 0;
+            int n;
+            Console.WriteLine("Введите числа (0 для выхода):");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                n = Convert.ToInt32(Console.ReadLine());
+                if (n > 0)
+                {
+                    pol = pol + 1;
+                }
+
+                if (n < 0)
+                {
+                    otr = otr + 1;
+                }
+
+            } while (n != 0);
+            Console.WriteLine("кол-во положительных: {0} кол-во отрицательных: {1}", pol, otr);
+            Console.ReadKey();
+        }
+    }
+}
