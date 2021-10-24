ebd6b35f5a6169f7e7779f407c90227611ca89c1
demidov_g@mail.ru <demidov_g@mail.ru>
2021-10-24 17:49:09 +0300

    1

--- Program.cs
+++ Program.cs
@@ -10,6 +10,25 @@ namespace Lab4
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите N>0:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int nk;
+            int n2;
+            
+            for (int i = 1; i <= n; )
+            {
+                n2 = 0;
+                nk = (i * 2) - 1;
+                for (int j = 1; j <= nk;)
+                {
+                    n2 = n2 + j;
+                    j = j + 2;
+                }
+                Console.WriteLine("число {0} квадрат {1}", i, n2);
+                i = i + 1;
+            }
+            Console.ReadKey();
+
         }
     }
 }
